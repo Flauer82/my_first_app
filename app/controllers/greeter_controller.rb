75fda8bef23c65848f5ff7a35ccd0a6427453d62
Lorenzo Fiorentino <lorenzo.fiorentino@gmail.com>
2016-03-07 19:06:13 +0100

    Routes configured

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,4 +1,10 @@
 class GreeterController < ApplicationController
   def hello
+  	@name = "Lorenzo"
+  	@time = Time.now
+  	@times_displayed ||= 0
+  	@times_displayed += 1
+  end
+  def goodbye
   end
 end
